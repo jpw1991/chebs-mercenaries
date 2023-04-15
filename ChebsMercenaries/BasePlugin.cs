aa62f0411013cf224d3dfb8e6aabf23a3400a8ec
Joshua Paul Woods <jpw1991@gmail.com>
2023-04-14 17:12:42 -0700

    DLL merging compatibility

--- ChebsMercenaries/BasePlugin.cs
+++ ChebsMercenaries/BasePlugin.cs
@@ -28,7 +28,7 @@ namespace ChebsMercenaries
         private const string ConfigFileName =  PluginGuid + ".cfg";
         private static readonly string ConfigFileFullPath = Path.Combine(Paths.ConfigPath, ConfigFileName);
 
-        public readonly System.Version ChebsValheimLibraryVersion = new("1.1.0");
+        public readonly System.Version ChebsValheimLibraryVersion = new("1.1.1");
 
         private readonly Harmony harmony = new(PluginGuid);
         
