2c2e1b7a8d6c904dd2e9f494c0d28afd048b1bc8
Joshua Paul Woods <jpw1991@gmail.com>
2023-05-25 15:58:06 -0700

    assembly info

--- ChebsMercenaries/Properties/AssemblyInfo.cs
+++ ChebsMercenaries/Properties/AssemblyInfo.cs
@@ -4,14 +4,16 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChebsMercenaries")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChebsMercenaries")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTitle("Cheb's Mercenaries")]
+[assembly: AssemblyDescription("Cheb's Mercenaries adds mercenaries to Valheim that you can purchase with gold and " +
+                               "upgrade with materials to fight (warriors, archers) or perform work (lumberjacks, " +
+                               "miners).")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Cheb Gonaz")]
+[assembly: AssemblyProduct("Cheb's Mercenaries")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
