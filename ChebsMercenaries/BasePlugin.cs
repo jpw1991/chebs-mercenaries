ebe2dcc6ccdb48690bf3046794604839f770a587
Joshua Paul Woods <jpw1991@gmail.com>
2024-02-09 11:07:07 +1100

    update CVL

--- ChebsMercenaries/BasePlugin.cs
+++ ChebsMercenaries/BasePlugin.cs
@@ -29,7 +29,7 @@ namespace ChebsMercenaries
         private const string ConfigFileName = PluginGuid + ".cfg";
         private static readonly string ConfigFileFullPath = Path.Combine(Paths.ConfigPath, ConfigFileName);
 
-        public readonly System.Version ChebsValheimLibraryVersion = new("2.5.2");
+        public readonly System.Version ChebsValheimLibraryVersion = new("2.5.5");
 
         private readonly Harmony harmony = new(PluginGuid);
 
